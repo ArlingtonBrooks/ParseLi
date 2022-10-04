cd030db1dfe77f0f9c48cbe47e50f820bc16176c
None Given <16168606+ArlingtonBrooks@users.noreply.github.com>
2022-10-04 18:38:13 -0400

    Updated GPL header

--- src/parselib.cpp
+++ src/parselib.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2019  ArlingtonBrooks
+    Copyright (C) 2019-2022  ArlingtonBrooks
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
